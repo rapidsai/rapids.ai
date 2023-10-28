b7e423f8ad488e6c8a4e085b0e886453576c9a65
AJ Schmidt <aschmidt@nvidia.com>
2023-10-28 21:39:53 +0000

    add docsy dependencies

--- scripts/devcontainer-init.sh
+++ scripts/devcontainer-init.sh
@@ -6,3 +6,11 @@ mkdir -p ~/.local/bin
 HUGO_VERSION=$(yq -oy '.build.environment.HUGO_VERSION' netlify.toml)
 wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" -O - | \
 tar -xzvf - -C ~/.local/bin hugo
+
+# Install Hugo/Go Modules
+hugo mod get
+
+# Install NodeJS version from `.nvmrc` file
+. "${NVM_DIR}/nvm.sh" --install
+rm -rf node_modules
+npm install
