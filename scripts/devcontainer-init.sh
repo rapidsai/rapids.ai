612011b4acd84b906d928ae6aa5caa387c15a900
AJ Schmidt <aschmidt@nvidia.com>
2024-01-05 12:53:28 +0000

    fix `devcontainer-init.sh` module download

--- scripts/devcontainer-init.sh
+++ scripts/devcontainer-init.sh
@@ -7,11 +7,8 @@ HUGO_VERSION=$(yq -oy '.build.environment.HUGO_VERSION' netlify.toml)
 wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" -O - | \
 tar -xzvf - -C ~/.local/bin hugo
 
-# Install Hugo/Go Modules
-# Dependency versions repeated here due to: https://github.com/gohugoio/hugo/issues/11857.
-# Once that is resolved, this command can be simplified to just:
-# hugo mod get
-hugo mod get github.com/google/docsy@v0.8.0
+# Install Hugo Modules
+hugo config
 
 # Install NodeJS version from `.nvmrc` file
 . "${NVM_DIR}/nvm.sh" --install
