53cf17db6998e0d8a0399b983dcf6e5cb79396ab
Ray Bell <rayjohnbell0@gmail.com>
2024-05-04 08:37:49 -0400

    rm solver=libmamba and add latest blog (#374)
    Co-authored-by: Ray Bell <ray.bell@dtn.com>
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- scripts/devcontainer-init.sh
+++ scripts/devcontainer-init.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # Install Hugo version from `netlify.toml` file
 mkdir -p ~/.local/bin
 HUGO_VERSION=$(yq -oy '.build.environment.HUGO_VERSION' netlify.toml)
-wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" -O - | \
-tar -xzvf - -C ~/.local/bin hugo
+wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" -O - |
+    tar -xzvf - -C ~/.local/bin hugo
 
 # Install Hugo Modules
 hugo config
@@ -14,3 +14,6 @@ hugo config
 . "${NVM_DIR}/nvm.sh" --install
 rm -rf node_modules
 npm install
+
+# Install the python libraries for get_medium.py
+pip install requests xmltodict pyyaml
