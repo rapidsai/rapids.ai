2c3cd0b669c185896c404e971be7e8f7ae40ef24
Allan Enemark <exactly.allan@gmail.com>
2024-06-18 20:56:45 +0000

    enable mac hardware for hugo build on dev container

--- scripts/devcontainer-init.sh
+++ scripts/devcontainer-init.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+case $(arch) in
+  "arm64"|"aarch64")
+    arch="arm64"
+    ;;
+  "amd64"|"x86_64")
+    arch="amd64"
+    ;;
+  *)
+    echo "Unsupported architecture: $(arch)"
+    exit 1
+    ;;
+esac
+
 # Install Hugo version from `netlify.toml` file
 mkdir -p ~/.local/bin
 HUGO_VERSION=$(yq -oy '.build.environment.HUGO_VERSION' netlify.toml)
-wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" -O - |
+wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-${arch}.tar.gz" -O - | \
     tar -xzvf - -C ~/.local/bin hugo
 
 # Install Hugo Modules
